d5fbf572dbc4a82f96a1a923d5be3a8532ab3bed
cacaedlsc <psalsabilla88@gmail.com>
2024-06-27 15:17:07 +0700

    nambah form & function register/sign up

---
+++ register.php
@@ -0,0 +1,241 @@
+<?php
+# Include connection
+require_once "./config.php";
+
+# Define variables and initialize with empty values
+$username_err = $email_err = $password_err = "";
+$username = $email = $password = "";
+
+# Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  # Validate username
+  if (empty(trim($_POST["username"]))) {
+    $username_err = "Please enter a username.";
+  } else {
+    $username = trim($_POST["username"]);
+    if (!ctype_alnum(str_replace(array("@", "-", "_"), "", $username))) {
+      $username_err = "Username can only contain letters, numbers and symbols like '@', '_', or '-'.";
+    } else {
+      # Prepare a select statement
+      $sql = "SELECT id FROM users WHERE username = ?";
+
+      if ($stmt = mysqli_prepare($link, $sql)) {
+        # Bind variables to the statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+        # Set parameters
+        $param_username = $username;
+
+        # Execute the prepared statement 
+        if (mysqli_stmt_execute($stmt)) {
+          # Store result
+          mysqli_stmt_store_result($stmt);
+
+          # Check if username is already registered
+          if (mysqli_stmt_num_rows($stmt) == 1) {
+            $username_err = "This username is already registered.";
+          }
+        } else {
+          echo "<script>" . "alert('Oops! Something went wrong. Please try again later.')" . "</script>";
+        }
+
+        # Close statement 
+        mysqli_stmt_close($stmt);
+      }
+    }
+  }
+
+  # Validate email 
+  if (empty(trim($_POST["email"]))) {
+    $email_err = "Please enter an email address";
+  } else {
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $email_err = "Please enter a valid email address.";
+    } else {
+      # Prepare a select statement
+      $sql = "SELECT id FROM users WHERE email = ?";
+
+      if ($stmt = mysqli_prepare($link, $sql)) {
+        # Bind variables to the statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_email);
+
+        # Set parameters
+        $param_email = $email;
+
+        # Execute the prepared statement 
+        if (mysqli_stmt_execute($stmt)) {
+          # Store result
+          mysqli_stmt_store_result($stmt);
+
+          # Check if email is already registered
+          if (mysqli_stmt_num_rows($stmt) == 1) {
+            $email_err = "This email is already registered.";
+          }
+        } else {
+          echo "<script>" . "alert('Oops! Something went wrong. Please try again later.');" . "</script>";
+        }
+
+        # Close statement
+        mysqli_stmt_close($stmt);
+      }
+    }
+  }
+
+  # Validate password
+  if (empty(trim($_POST["password"]))) {
+    $password_err = "Please enter a password.";
+  } else {
+    $password = trim($_POST["password"]);
+    if (strlen($password) < 8) {
+      $password_err = "Password must contain at least 8 or more characters.";
+    }
+  }
+
+  # Check input errors before inserting data into database
+  if (empty($username_err) && empty($email_err) && empty($password_err)) {
+    # Prepare an insert statement
+    $sql = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+
+    if ($stmt = mysqli_prepare($link, $sql)) {
+      # Bind varibales to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_email, $param_password);
+
+      # Set parameters
+      $param_username = $username;
+      $param_email = $email;
+      $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+      # Execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+        echo "<script>" . "alert('Registeration completed successfully. Login to continue.');" . "</script>";
+        echo "<script>" . "window.location.href='./login.php';" . "</script>";
+        exit;
+      } else {
+        echo "<script>" . "alert('Oops! Something went wrong. Please try again later.');" . "</script>";
+      }
+
+      # Close statement
+      mysqli_stmt_close($stmt);
+    }
+  }
+
+  # Close connection
+  mysqli_close($link);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cara Coustic</title>
+  <link rel="icon" type="image/png" href="img/cara-title.png">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="./css/main.css">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="shortcut icon" href="./img/favicon-16x16.png" type="image/x-icon">
+  <script defer src="./js/script.js"></script>
+</head>
+
+<body>
+<section id="header">
+            <a href="#"><img src="img/cara2.png" alt="" class="logo" alt=""></a>
+
+            <div>
+                <ul id="navbar">
+                    <li><a class="active" href="index.html">Home</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="new.html">New</a></li>
+                    <li><a href="second.html">Second</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li id="lg-bag"><a href="cart.html"><i class="far fa-shopping-bag"></i></a></li>
+                    <a href="#" id="close"><i class="far fa-times"></i></a>
+                </ul>
+            </div>
+            <div id="mobile">
+                <a href="cart.html"><i class="far fa-shopping-bag"></i></i></a>
+                <i id="bar" class="fas fa-outdent"></i>
+            </div>
+</section>
+
+  <div class="container">
+    <div class="row min-vh-100 justify-content-center align-items-center">
+      <div class="col-lg-5">
+        <div class="form-wrap border rounded p-4">
+          <h1>Sign up</h1>
+          <p>Please fill this form to register</p>
+          <!-- form starts here -->
+          <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" novalidate>
+            <div class="mb-3">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" class="form-control" name="username" id="username" value="<?= $username; ?>">
+              <small class="text-danger"><?= $username_err; ?></small>
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email Address</label>
+              <input type="email" class="form-control" name="email" id="email" value="<?= $email; ?>">
+              <small class="text-danger"><?= $email_err; ?></small>
+            </div>
+            <div class="mb-2">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" name="password" id="password" value="<?= $password; ?>">
+              <small class="text-danger"><?= $password_err; ?></small>
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="togglePassword">
+              <label for="togglePassword" class="form-check-label">Show Password</label>
+            </div>
+            <div class="mb-3">
+              <input type="submit" class="btn btn-primary form-control" name="submit" value="Sign Up">
+            </div>
+            <p class="mb-0">Already have an account ? <a href="login.php">Log In</a></p>
+          </form>
+          <!-- form ends here -->
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- SECTION FOOTER -->
+  <footer class="section-p1">
+            <div class="col">
+                <img class="logo" src="img/cara2.png" alt="">
+                <h4>Contact</h4>
+                <p><strong>Address: </strong>Merbabu Raya No. 504, Kota Semarang, Jawa Tengah</p>
+                <p><strong>Phone: </strong>(024) 98725 / (024) 25938</p>
+                <p><strong>Hours: </strong>08.00 - 20.00, Mon - Sat</p>
+                <div class="follow">
+                    <h4>Follow Us</h4>
+                    <div class="icon">
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-pinterest-p"></i>
+                        <i class="fab fa-youtube"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <h4>My Account</h4>
+                <a href="#">Sign In</a>
+                <a href="#">View Cart</a>
+                <a href="#">My Wishlist</a>
+                <a href="#">Track My Order</a>
+                <a href="#">Help</a>
+            </div>
+
+            <div class="copyright">
+                <p>© 2024, Salsa E - 2201530047</p>
+            </div>
+    </footer>
+</body>
+
+</html>
