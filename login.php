4a90c8e47f15846c30b5874b87be3d40425febe8
cacaedlsc <psalsabilla88@gmail.com>
2024-06-27 17:03:32 +0700

    menyesuaikan UI

--- login.php
+++ login.php
@@ -91,14 +91,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>User login system</title>
+  <title>Cara Coustic</title>
+  <link rel="icon" type="image/png" href="img/cara-title.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="./css/main.css">
-  <link rel="shortcut icon" href="./img/favicon-16x16.png" type="image/x-icon">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="shortcut icon" href="./img/cara-title.png" type="image/x-icon">
   <script defer src="./js/script.js"></script>
 </head>
 
 <body>
+<section id="header">
+            <a href="#"><img src="img/cara2.png" alt="" class="logo" alt=""></a>
+
+            <div>
+                <ul id="navbar">
+                    <li><a class="active" href="index.html">Home</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="new.html">New</a></li>
+                    <li><a href="second.html">Second</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li id="lg-bag"><a href="cart.html"><i class="far fa-shopping-bag"></i></a></li>
+                    <a href="#" id="close"><i class="far fa-times"></i></a>
+                </ul>
+            </div>
+            <div id="mobile">
+                <a href="cart.html"><i class="far fa-shopping-bag"></i></i></a>
+                <i id="bar" class="fas fa-outdent"></i>
+            </div>
+</section>
+
+
   <div class="container">
     <div class="row min-vh-100 justify-content-center align-items-center">
       <div class="col-lg-5">
@@ -136,6 +161,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       </div>
     </div>
   </div>
-</body>
+   <!-- SECTION FOOTER -->
+   <footer class="section-p1">
+            <div class="col">
+                <img class="logo" src="img/cara2.png" alt="">
+                <h4>Contact</h4>
+                <p><strong>Address: </strong>Merbabu Raya No. 504, Kota Semarang, Jawa Tengah</p>
+                <p><strong>Phone: </strong>(024) 98725 / (024) 25938</p>
+                <p><strong>Hours: </strong>08.00 - 20.00, Mon - Sat</p>
+                <div class="follow">
+                    <h4>Follow Us</h4>
+                    <div class="icon">
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-pinterest-p"></i>
+                        <i class="fab fa-youtube"></i>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col">
+                <h4>My Account</h4>
+                <a href="#">Sign In</a>
+                <a href="#">View Cart</a>
+                <a href="#">My Wishlist</a>
+                <a href="#">Track My Order</a>
+                <a href="#">Help</a>
+            </div>
+
+            <div class="copyright">
+                <p>© 2024, Salsa E - 2201530047</p>
+            </div>
+    </footer>
+</body>
 </html>
